c1fafff2a3cb9d356dac4d6799010f980a2c53bd
Dilan EESHVARAN <ieeshvarand@gmail.com>
2026-02-06 17:05:10 +0100

    project init finished- front setup completed - (ready to dev)

---
+++ frontend/app/layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex min-h-screen flex-col">
+    <!-- todo :  header/navbar  -->
+    <main class="flex-1">
+      <slot />
+    </main>
+    <!-- todo:  footer  -->
+  </div>
+</template>
---
+++ frontend/app/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex items-center justify-center min-h-screen">
+    <h1 class="text-4xl font-bold text-primary-600">
+      Bienvenue sur Medicote
+    </h1>
+  </div>
+</template>
