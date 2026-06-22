a5e21327c1271fc6033a63345955f9c4a6237ffb
Dilan EESHVARAN <ieeshvarand@gmail.com>
2026-06-22 19:56:38 +0200

    notifications

--- backend/src/modules/notifications/notifications.types.ts
+++ backend/src/modules/notifications/notifications.types.ts
@@ -1,9 +1,11 @@
+import type { Prisma } from '@prisma/client'
+
 export interface UserNotification {
   id: string
   type: string
   title: string
   message: string
-  metadata: any
+  metadata: Prisma.JsonValue | null
   read: boolean
   readAt: Date | null
   createdAt: Date
